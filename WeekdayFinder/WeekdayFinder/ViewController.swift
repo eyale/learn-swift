91aa113330774c6a90c3e999edd6e58e9c1c0f06
eyale <4eyalush@gmail.com>
2020-04-05 13:19:23 +0300

    adding WeekDayFinder

---
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Anton Goncharov on 05.04.2020.
+//  Copyright © 2020 Anton Honcharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var dayTF: UITextField!
+    @IBOutlet var monthTF: UITextField!
+    @IBOutlet var yearTF: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findDay() {
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
