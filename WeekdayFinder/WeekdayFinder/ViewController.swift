54b8b78018548b47f3437b8bf875123e7c0651c7
eyale <4eyalush@gmail.com>
2020-04-06 11:23:39 +0300

    constraints and layout

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -23,7 +23,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 }
 
