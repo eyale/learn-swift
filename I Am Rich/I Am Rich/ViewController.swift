03c20decfb72b89667b01dabf67d29d0feb937a5
eyale <4eyalush@gmail.com>
2020-04-25 09:28:51 +0300

    more two simple apps

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anton Goncharov on 21.04.2020.
+//  Copyright © 2020 Anton Honcharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
