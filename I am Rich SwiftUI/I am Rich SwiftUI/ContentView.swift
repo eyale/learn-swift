38ccbe3ae3da3f1df5072edf3ece032c7ad26c52
eyale <4eyalush@gmail.com>
2020-07-05 21:29:07 +0300

    adding I am Rich SwiftUI app

---
+++ I am Rich SwiftUI/I am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich SwiftUI
+//
+//  Created by Anton Goncharov on 29.06.2020.
+//  Copyright © 2020 Anton Honcharov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice("iPad Pro (11-inch)")
+    }
+}
